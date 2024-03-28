3d4a003f81eff8ed9925ffe1c8fb210bcd209320
William Riser <riser.w@northeastern.edu>
2024-03-28 15:56:37 -0700

    remove dump

--- query1.js
+++ query1.js
@@ -1,20 +1,18 @@
-import { MongoClient } from 'mongodb';
+import { read } from "fs";
+import { MongoClient } from "mongodb";
 
-const MONGO_URL = process.env.MONGO_URL || 'mongodb://localhost:27017';
+const MONGO_URL = process.env.MONGO_URL || "mongodb://localhost:27017";
 
-const client = await MongoClient.connect(
-  MONGO_URL,
-);
-const coll = client.db('ieeevisTweets').collection('tweet');
-
+const client = await MongoClient.connect(MONGO_URL);
+const coll = client.db("ieeevisTweets").collection("tweet");
 
 const filter = {
-  'retweet_count': 6
+  retweet_count: 6,
 };
 
 const cursor = coll.find(filter);
 const result = await cursor.toArray();
 
-console.log(result.length + ' tweets found');
+console.log(result);
 
 await client.close();
